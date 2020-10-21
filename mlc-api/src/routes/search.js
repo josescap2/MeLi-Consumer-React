3f2c3284f96e0eaaed4f93046144ad0c522192d0
Juan Jose Puente Scapolatempo <juanpuentescapoltempo@gmail.com>
2020-10-21 15:20:41 -0300

    api funcionando

--- mlc-api/src/routes/search.js
+++ mlc-api/src/routes/search.js
@@ -1,9 +1,13 @@
 const server = require("express").Router();
+const fetch = require("node-fetch");
 
-server.get('/', (req, res) => {
-  res.send({
-    message: "Hello world"
-  });
+server.get('/search', (req, res) => {
+  const { query } = req.query;
+
+  fetch("https://api.mercadolibre.com/sites/MLA/search?q=" + query)
+    .then(results => results.json())
+    .then(data => res.json(data))
+    .catch(err => res.send(err));
 });
 
 module.exports = server;
