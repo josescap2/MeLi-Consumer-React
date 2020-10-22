7d8526dd49f6df40c0217ea081448722f889a2a3
Juan Jose Puente Scapolatempo <juanpuentescapoltempo@gmail.com>
2020-10-22 15:36:34 -0300

    Paginador

--- mlc-client/src/components/Catalog/index.jsx
+++ mlc-client/src/components/Catalog/index.jsx
@@ -1,24 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ProductCard from './../ProductCard';
+import Pager from './pager.jsx';
 import { Grid } from '@material-ui/core';
 import { v4 } from 'uuid';
 
 export default function({ products = []}) {
+  const [page, setPage] = useState(0);
+
   return (
     <div>
       <Grid container justify="space-between">
         {
-          products.length > 0 ? products.map((product) => (
-            <Grid item lg={4} key={v4()}>
-              <ProductCard
-                title={product.title}
-                money={product.money}
-                price={product.price}
-                stock={product.stock}
-                image={product.image}
-                permalink={product.link}
-              />
-            </Grid>
+          products.length > 0 ? products.map((product, index) => (
+            index >= (page) * 10 && index < page + 1 * 10 ? (
+              <Grid item lg={6} key={v4()}>
+                <ProductCard
+                  title={product.title}
+                  money={product.money}
+                  price={product.price}
+                  stock={product.stock}
+                  image={product.image}
+                  permalink={product.link}
+                />
+              </Grid>
+            ) : (
+              <React.Fragment>
+              </React.Fragment>
+            )
           )) : (
             <React.Fragment>
               <Grid item>
@@ -28,6 +36,13 @@ export default function({ products = []}) {
           )
         }
       </Grid>
+      {
+        products.length > 0 ? (
+          <Pager />
+        ) : (
+          <React.Fragment></React.Fragment>
+        )
+      }
     </div>
   );
 }
