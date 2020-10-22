103394934834fe4ffb005e64b96aab7aa100d680
Juan Jose Puente Scapolatempo <juanpuentescapoltempo@gmail.com>
2020-10-21 22:54:40 -0300

    App

--- mlc-client/src/App.js
+++ mlc-client/src/App.js
@@ -1,10 +1,16 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
+import SearchBar from './components/SearchBar';
+import Catalog from './components/Catalog';
 
 function App() {
+  // HOOKS
+  const [products, setProducts] = useState([]);
+
   return (
     <div>
-      
+      <SearchBar setProducts={setProducts}/>
+      <Catalog products={products}/>
     </div>
   );
 }
